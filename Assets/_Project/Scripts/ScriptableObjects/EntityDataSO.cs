e6c511fcc3337368073a6dda4fa66f7deba0dfe4
KoyFC <162127086+KoyFC@users.noreply.github.com>
2026-06-06 18:15:10 +0200

    Add EntityDataSO

---
+++ Assets/_Project/Scripts/ScriptableObjects/EntityDataSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Entity Data", menuName = "Scriptable Objects/Entity Data")]
+public class EntityDataSO : ScriptableObject
+{
+    [Header("Entity")]
+    [field: SerializeField] public string Name { get; private set; }
+    [field: SerializeField] public Sprite Sprite { get; private set; }
+    [field: SerializeField] public EntityBase Prefab { get; private set; }
+}
