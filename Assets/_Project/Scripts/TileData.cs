1f0dff5a61193c60b761cea012feda679ebf4ee9
KoyFC <162127086+KoyFC@users.noreply.github.com>
2026-06-06 13:16:37 +0200

    Add Tile Resources

--- Assets/_Project/Scripts/TileData.cs
+++ Assets/_Project/Scripts/TileData.cs
@@ -5,9 +5,24 @@ public class TileData
     public Vector2Int TileIndex { get; private set; }
     public Vector3 WorldPosition { get; private set; }
 
+    public float Humidity { get; private set; } = 0f; // 0 to 100
+    public float Nutrients { get; private set; } = 0f; // 0 to 100
+
     public TileData(Vector2Int tileIndex, Vector3 worldPosition)
     {
         TileIndex = tileIndex;
         WorldPosition = worldPosition;
     }
+
+    #region Public Methods
+    public void AddHumidity(float amount)
+    {
+        Humidity = Mathf.Clamp(Humidity + amount, 0f, 100f);
+    }
+
+    public void AddNutrients(float amount)
+    {
+        Nutrients = Mathf.Clamp(Nutrients + amount, 0f, 100f);
+    }
+    #endregion
 }
