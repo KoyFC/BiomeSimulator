77615dd86cb38f68dd5e80a2825d5f790fdf9970
KoyFC <162127086+KoyFC@users.noreply.github.com>
2026-06-08 10:52:57 +0200

    Only Update Billboard on first frame

--- Assets/_Project/Scripts/BillboardScript.cs
+++ Assets/_Project/Scripts/BillboardScript.cs
@@ -3,13 +3,24 @@ using UnityEngine;
 public class BillboardScript : MonoBehaviour
 {
     private Transform m_CameraTransform;
+    private bool m_OnlyUpdateFirstFrame = true;
 
     private void Start()
     {
         m_CameraTransform = Camera.main.transform;
+        if (m_OnlyUpdateFirstFrame)
+        {
+            LookAtCamera();
+            enabled = false;
+        }
     }
 
     private void LateUpdate()
+    {
+        LookAtCamera();
+    }
+
+    private void LookAtCamera()
     {
         if (m_CameraTransform == null) return;
 
